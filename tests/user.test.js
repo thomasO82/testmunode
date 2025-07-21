938ba46762a5b7e17c8b38d734d9cd41d83b8744
thomasO82 <oliver-thomas@live.fr>
2025-07-21 11:32:42 +0200

    dfdfgfg

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,20 +1,24 @@
 const request = require("supertest");
 const app = require("../src/server.js");
 
-it("create user with good value" , async()=>{
-    const res = await request(app)
-    .post('/users')
-    .send({name: "flo", email: "flo@live.fr"})
-    expect(res.statusCode).toBe(201)
-    expect(res.body).toHaveProperty('id')
-    expect(res.body.name).toBe("flo")
-})
 
-it("create user with bad value" , async()=>{
-    const res = await request(app)
-    .post('/users')
-    .send({ email: "flo@live.fr"})
-    expect(res.statusCode).toBe(400)
-    expect(res.body.error).toBe('Missing fields')
-   
+describe('Pack test user post', () => {
+    it("create user with good value", async () => {
+        const res = await request(app)
+            .post('/users')
+            .send({ name: "flo", email: "flo@live.fr" })
+        expect(res.statusCode).toBe(201)
+        expect(res.body).toHaveProperty('id')
+        expect(res.body.name).toBe("flo")
+    })
+
+    it("create user with bad value", async () => {
+        const res = await request(app)
+            .post('/users')
+            .send({ email: "flo@live.fr" })
+        expect(res.statusCode).toBe(400)
+        expect(res.body.error).toBe('Missing fields')
+
+    })
+
 })
